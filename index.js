0c330dd6489091af08e5cb32c185f5730007478d
Jaclyn Hall <jaclynchall@gmail.com>
2017-12-28 21:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   if (string === string.toUpperCase())
     return "YES INDEED!"
-  if (string === console.log('I love you, Grandma.'))
+  if (string === "I love you, Grandma.")
     return "I love you, too."
 }
